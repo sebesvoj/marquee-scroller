04d7be53f2ec76442088b931c8fa41b622bbbd07
sebesvoj <vojta.sebesta@gmail.com>
2024-01-22 07:23:26 +0100

    up

--- marquee/Settings.h
+++ marquee/Settings.h
@@ -82,7 +82,9 @@ String NEWS_SOURCE = "reuters";  // https://newsapi.org/sources to get full list
 // CLK -> D5 (SCK)  
 // CS  -> D6 
 // DIN -> D7 (MOSI)
-const int pinCS = D6; // Attach CS to this pin, DIN to MOSI and CLK to SCK (cf http://arduino.cc/en/Reference/SPI )
+
+const int pinCS = D2; // Attach CS to this pin, DIN to MOSI and CLK to SCK (cf http://arduino.cc/en/Reference/SPI )
+//const int pinCS = D6; // Attach CS to this pin, DIN to MOSI and CLK to SCK (cf http://arduino.cc/en/Reference/SPI )
 int displayIntensity = 1;  //(This can be set from 0 - 15)
 const int numberOfHorizontalDisplays = 4; // default 4 for standard 4 x 1 display Max size of 16
 const int numberOfVerticalDisplays = 1; // default 1 for a single row height
